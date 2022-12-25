ac860be856710a154247e8e7e7a03f3a2070a0bd
Stepan <stepan.karpov@list.ru>
2022-12-25 21:50:01 +0300

    ReduceFraction upgraded

--- matrix.h
+++ matrix.h
@@ -27,8 +27,8 @@ std::istream& operator>>(std::istream&, BigInteger&);
 
 class BigInteger {
  private:
-  std::vector<int> digits_;
-  short sign_ = 1;
+  std::vector<int> arr;
+  short sign = 1;
   static const int BASE_ = 1'000'000'000;
   static const int MAX_DIGITS_ = 9; // 9 is log10(BASE_)
   // 1 is positive or 0
@@ -54,29 +54,29 @@ class BigInteger {
   }
 
   void DeleteLeadingZeros() {
-    while (!digits_.empty() && digits_[digits_.size() - 1] == 0) {
-      digits_.pop_back();
+    while (!arr.empty() && arr[arr.size() - 1] == 0) {
+      arr.pop_back();
     }
   }
 
   BigInteger& FindSum(const BigInteger& addition) {
     // since now both values are positive
-    while (digits_.size() < addition.digits_.size()) {
-      digits_.push_back(0);
+    while (arr.size() < addition.arr.size()) {
+      arr.push_back(0);
     }
-    digits_.push_back(0);
-    for (int i = 0; i < int(digits_.size()); ++i) {
+    arr.push_back(0);
+    for (int i = 0; i < int(arr.size()); ++i) {
       int cur_addition = 0;
-      if (i < int(addition.digits_.size())) {
-        cur_addition = addition.digits_[i];
+      if (i < int(addition.arr.size())) {
+        cur_addition = addition.arr[i];
       }
-      digits_[i] += cur_addition;
+      arr[i] += cur_addition;
     }
     int in_mind = 0;
-    for (int i = 0; i < int(digits_.size()); ++i) {
-      digits_[i] += in_mind;
-      in_mind = digits_[i] / BASE_;
-      digits_[i] %= BASE_;
+    for (int i = 0; i < int(arr.size()); ++i) {
+      arr[i] += in_mind;
+      in_mind = arr[i] / BASE_;
+      arr[i] %= BASE_;
     }
     this->DeleteLeadingZeros();
     return *this;
@@ -84,21 +84,21 @@ class BigInteger {
 
   BigInteger& FindDifference(const BigInteger& addition) {
     // since now both values are positive ans this - addition >= 0
-    for (int i = 0; i < int(digits_.size()); ++i) {
-      if (i >= int(addition.digits_.size())) { break; }
-      if (digits_[i] < addition.digits_[i]) {
+    for (int i = 0; i < int(arr.size()); ++i) {
+      if (i >= int(addition.arr.size())) { break; }
+      if (arr[i] < addition.arr[i]) {
         int j = i + 1;
-        while (digits_[j] == 0) {
+        while (arr[j] == 0) {
           ++j;
         }
-        --digits_[j];
+        --arr[j];
         for (int k = i; k < j; ++k) {
-          digits_[k] += BASE_ - 1;
+          arr[k] += BASE_ - 1;
         }
-        ++digits_[i];
-        digits_[i] -= addition.digits_[i];
+        ++arr[i];
+        arr[i] -= addition.arr[i];
       } else {
-        digits_[i] -= addition.digits_[i];
+        arr[i] -= addition.arr[i];
       }
     }
     this->DeleteLeadingZeros();
@@ -107,12 +107,12 @@ class BigInteger {
 
   // returns true if |a| > |b|
   bool ModuloComparison(const BigInteger& a, const BigInteger& b) {
-    if (b.digits_.size() != a.digits_.size()) {
-      return (b.digits_.size() < a.digits_.size());
+    if (b.arr.size() != a.arr.size()) {
+      return (b.arr.size() < a.arr.size());
     }
-    for (int i = int(b.digits_.size()) - 1; i >= 0; --i) {
-      if (b.digits_[i] != a.digits_[i]) {
-        return (b.digits_[i] < a.digits_[i]);
+    for (int i = int(b.arr.size()) - 1; i >= 0; --i) {
+      if (b.arr[i] != a.arr[i]) {
+        return (b.arr[i] < a.arr[i]);
       }
     }
     return false;
@@ -124,10 +124,10 @@ class BigInteger {
   friend BigInteger FindGCD(BigInteger a, BigInteger b);
   
   BigInteger(long long x) {
-    sign_ = (x >= 0 ? 1 : -1);
+    sign = (x >= 0 ? 1 : -1);
     x = abs(x);
     while (x != 0) {
-      digits_.push_back(x % BASE_);
+      arr.push_back(x % BASE_);
       x /= BASE_;
     }
   }
@@ -196,8 +196,8 @@ class BigInteger {
     BigInteger temp = addition;
     temp.FindDifference(*this);
     *this = temp;
-    if (int(digits_.size()) != 0) {
-      this->sign_ = -this->sign_;
+    if (int(arr.size()) != 0) {
+      this->sign = -this->sign;
     }
     return *this;
   }
@@ -208,14 +208,14 @@ class BigInteger {
       *this = 0;
       return *this;
     }
-    int v1 = digits_.size();
-    int v2 = multiplier.digits_.size();
+    int v1 = arr.size();
+    int v2 = multiplier.arr.size();
     std::vector<long long> answer(v1 + v2 + 2, 0);
     for (int i = 0; i < v2; ++i) {
       int cur_pos = i;
       for (int j = 0; j < v1; ++j) {
-        long long temp = digits_[j];
-        temp *= multiplier.digits_[i];
+        long long temp = arr[j];
+        temp *= multiplier.arr[i];
         answer[cur_pos] += temp;
         ++cur_pos;
       }
@@ -226,11 +226,11 @@ class BigInteger {
         answer[j] %= BASE_;
       }
     }
-    digits_.resize(v1 + v2 + 2);
-    std::copy(answer.begin(), answer.end(), digits_.begin());
+    arr.resize(v1 + v2 + 2);
+    std::copy(answer.begin(), answer.end(), arr.begin());
     this->DeleteLeadingZeros();
-    if (multiplier.sign_ == -1) {
-      sign_ = -sign_;
+    if (multiplier.sign == -1) {
+      sign = -sign;
     }
     return *this;
   }
@@ -241,36 +241,39 @@ class BigInteger {
       *this = 0;
       return *this;
     }
+    if (divider_const == 1) {
+      return *this;
+    }
 
-    int s1 = sign_;
+    int s1 = sign;
     BigInteger divider = divider_const;
-    int s2 = divider_const.sign_;
-    sign_ = 1;
-    divider.sign_ = 1;
+    int s2 = divider_const.sign;
+    sign = 1;
+    divider.sign = 1;
 
     // since now code works like both values are positive
     std::vector<int> answer;
-    BigInteger t = digits_[digits_.size() - 1];
-    for (int i = (digits_.size() - 2); i >= -1; --i) {
+    BigInteger t = arr[arr.size() - 1];
+    for (int i = (arr.size() - 2); i >= -1; --i) {
       long long min_mult = FindMaxMult(t, divider);
       BigInteger max_value = divider * min_mult;
       answer.push_back(min_mult);
       t -= max_value;
       if (i >= 0) {
         t *= BASE_;
-        t += digits_[i];
+        t += arr[i];
       }
     }
-    digits_.clear();
+    arr.clear();
     for (int i = int(answer.size()) - 1; i >= 0; --i) {
-      digits_.push_back(answer[i]);
+      arr.push_back(answer[i]);
     }
-    while (!digits_.empty() && digits_[digits_.size() - 1] == 0) {
-      digits_.pop_back();
+    while (!arr.empty() && arr[arr.size() - 1] == 0) {
+      arr.pop_back();
     }
     // sign corrections
     if (s1 * s2 == -1) {
-      sign_ = -1;
+      sign = -1;
     }
     return *this;
   }
@@ -284,10 +287,10 @@ class BigInteger {
   
   BigInteger operator-() const {
     BigInteger answer = *this;
-    if (int(digits_.size()) == 0) {
+    if (int(arr.size()) == 0) {
       return answer;
     }
-    answer.sign_ = -(answer.sign_);
+    answer.sign = -(answer.sign);
     return answer;
   }
 
@@ -328,11 +331,11 @@ class BigInteger {
 
   explicit operator long long() const {
     long long x = 0;
-    for (int i = int(digits_.size()) - 1; i >= 0; --i) {
+    for (int i = int(arr.size()) - 1; i >= 0; --i) {
       x *= BASE_;
-      x += digits_[i];
+      x += arr[i];
     }
-    if (sign_ == -1) {
+    if (sign == -1) {
       x = -x;
     }
     return x;
@@ -372,25 +375,25 @@ BigInteger operator""_bi(const char* x) {
 }
 
 bool operator<(const BigInteger& value1, const BigInteger& value2) {
-  if (value1.sign_ == -1 && value2.sign_ == 1) {
+  if (value1.sign == -1 && value2.sign == 1) {
     return true;
   }
-  if (value1.sign_ == 1 && value2.sign_ == -1) {
+  if (value1.sign == 1 && value2.sign == -1) {
     return false;
   }
-  bool reverse = (value1.sign_ == -1 && value1.sign_ == -1);
-  if (value1.digits_.size() != value2.digits_.size()) {
+  bool reverse = (value1.sign == -1 && value1.sign == -1);
+  if (value1.arr.size() != value2.arr.size()) {
     if (reverse) {
-      return (value1.digits_.size() > value2.digits_.size());
+      return (value1.arr.size() > value2.arr.size());
     }
-    return (value1.digits_.size() < value2.digits_.size());
+    return (value1.arr.size() < value2.arr.size());
   }
-  for (int i = int(value1.digits_.size()) - 1; i >= 0; --i) {
-    if (value1.digits_[i] != value2.digits_[i]) {
+  for (int i = int(value1.arr.size()) - 1; i >= 0; --i) {
+    if (value1.arr[i] != value2.arr[i]) {
       if (reverse) {
-        return (value1.digits_[i] > value2.digits_[i]);
+        return (value1.arr[i] > value2.arr[i]);
       }
-      return (value1.digits_[i] < value2.digits_[i]);
+      return (value1.arr[i] < value2.arr[i]);
     }
   }
   return false;
@@ -433,11 +436,11 @@ bool operator<=(const BigInteger& value1, const BigInteger& value2) {
 }
 
 bool operator==(const BigInteger& value1, const BigInteger& value2) {
-  if (value1.digits_.size() != value2.digits_.size()) {
+  if (value1.arr.size() != value2.arr.size()) {
     return false;
   }
-  for (int i = 0; i < int(value1.digits_.size()); ++i) {
-    if (value1.digits_[i] != value2.digits_[i]) {
+  for (int i = 0; i < int(value1.arr.size()); ++i) {
+    if (value1.arr[i] != value2.arr[i]) {
       return false;
     }
   }
@@ -449,20 +452,20 @@ bool operator!=(const BigInteger& value1, const BigInteger& value2) {
 }
 
 std::ostream& operator<<(std::ostream& output, const BigInteger& value) {
-  if (value.digits_.size() == 0) {
+  if (value.arr.size() == 0) {
     output << 0;
     return output;
   }
-  if (value.sign_ == -1) {
+  if (value.sign == -1) {
     output << "-";
   }
-  if (value.digits_.empty()) {
+  if (value.arr.empty()) {
     std::cout << "0";
     return output;
   }
-  for (int i = int(value.digits_.size()) - 1; i >= 0; --i) {
-    std::string cur_digit = std::to_string(value.digits_[i]);
-    if (i != int(value.digits_.size()) - 1) {
+  for (int i = int(value.arr.size()) - 1; i >= 0; --i) {
+    std::string cur_digit = std::to_string(value.arr[i]);
+    if (i != int(value.arr.size()) - 1) {
       while (int(cur_digit.size()) < BigInteger::MAX_DIGITS_) {
         cur_digit = "0" + cur_digit;
       }
@@ -477,16 +480,16 @@ std::istream& operator>>(std::istream& input, BigInteger& value) {
   std::string s;
   input >> s;
   if (int(s.size() > 0)) {
-    value.sign_ = (s[0] == '-' ? -1 : 1);
+    value.sign = (s[0] == '-' ? -1 : 1);
   } else {
-    value.sign_ = 1;
+    value.sign = 1;
   }
-  value.digits_.clear();
+  value.arr.clear();
   std::string cur_digit = "";
   for (int i = int(s.size()) - 1; i >= (s[0] == '-'); --i) {
     if (int(cur_digit.size()) == BigInteger::MAX_DIGITS_) {
       std::reverse(cur_digit.begin(), cur_digit.end());
-      value.digits_.push_back(stoi(cur_digit));
+      value.arr.push_back(stoi(cur_digit));
       cur_digit = s[i];
     } else {
       cur_digit += s[i];
@@ -494,38 +497,51 @@ std::istream& operator>>(std::istream& input, BigInteger& value) {
   }
   if (cur_digit != "") {
     std::reverse(cur_digit.begin(), cur_digit.end());
-      value.digits_.push_back(stoi(cur_digit));
+      value.arr.push_back(stoi(cur_digit));
   }
   return input;
 }
 
+BigInteger FindGCD(BigInteger a, BigInteger b) {
+  if (a == 0 || b == 0) {
+    return 0;
+  }
+  a.sign = 1;
+  b.sign = 1;
+
+  BigInteger ans = 1;
+  while (a != b) {
+    bool ab = a.arr[0] % 2 == 0, bb = b.arr[0] % 2 == 0;
+    if (ab && bb) {
+      a /= 2;
+      b /= 2;
+      ans *= 2;
+    } else if (ab) {
+      a /= 2;
+    } else if (bb) {
+      b /= 2;
+    } else {
+      if (a > b) {
+        a -= b;
+      } else {
+        b -= a;
+      }
+    }
+  }
+  return ans * a;
+}
+
 // BigInteger FindGCD(BigInteger a, BigInteger b) {
 //   if (a == 0 || b == 0) {
-//     return 1;
+//     return 0;
 //   }
-//   a.sign_ = 1;
-//   b.sign_ = 1;
-
-//   BigInteger ans = 1;
-//   while (a != b) {
-//     bool ab = a.digits_[0] % 2 == 0, bb = b.digits_[0] % 2 == 0;
-//     if (ab && bb) {
-//       a /= 2;
-//       b /= 2;
-//       ans *= 2;
-//     } else if (ab) {
-//       a /= 2;
-//     } else if (bb) {
-//       b /= 2;
-//     } else {
-//       if (a > b) {
-//         a -= b;
-//       } else {
-//         b -= a;
-//       }
-//     }
+  
+//   while (b != 0) {
+//     BigInteger temp = a % b;
+//     a = b;
+//     b = temp;
 //   }
-//   return ans * a;
+//   return a;
 // }
 
 class Rational {
@@ -541,18 +557,26 @@ class Rational {
   static const int DEFAULT_PRECISION_ = 20;
   
   void ReduceFraction() {
-    BigInteger gcd = FindGCD(numerator_, denominator_);
-    numerator_ /= gcd;
-    denominator_ /= gcd;
-  }
-
-  static BigInteger FindGCD(BigInteger a, BigInteger b) {
-    while (b != 0) {
-      BigInteger temp = a % b;
-      a = b;
-      b = temp;
+    if (numerator_ == 0) {
+      denominator_ = 1;
+      return;
     }
-    return a;
+    if (numerator_ == 1) {
+      denominator_ = 1;
+      return;
+    }
+    if (numerator_ == denominator_) {
+      numerator_ = 1;
+      denominator_ = 1;
+      return;
+    }
+
+    BigInteger gcd = FindGCD(numerator_, denominator_);
+
+    if (gcd > 1) {
+      numerator_ /= gcd;
+      denominator_ /= gcd;
+    }
   }
 
  public:
